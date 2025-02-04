4b45fdeaf9725f4495d30b2a17359a34c63f7220
jacobproffer <jacobhardingproffer@gmail.com>
2025-02-04 12:23:04 -0500

    Add redirect for '/about/' to root URL

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,6 +8,9 @@ import sitemap from "@astrojs/sitemap";
 // https://astro.build/config
 export default defineConfig({
   site: "https://proffer.dev/",
+  redirects: {
+    '/about/': '/',
+  },
   integrations: [sitemap()],
   experimental: {
     responsiveImages: true,
