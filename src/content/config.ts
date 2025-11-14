77179d45b1b62fe7190bbe20c8b210922b0e8c76
jacobproffer <jacobhardingproffer@gmail.com>
2025-11-14 09:22:05 -0500

    Add experience collection and new experience entries for Elegant Seagulls and Sullivan

--- src/content/config.ts
+++ src/content/config.ts
@@ -13,4 +13,19 @@ const projects = defineCollection({
     }),
 });
 
-export const collections = { projects };
+const experience = defineCollection({
+  loader: glob({ pattern: "**/*.md", base: "./src/content/experience" }),
+  schema: z.object({
+    company: z.string(),
+    positions: z.array(
+      z.object({
+        position: z.string(),
+        startDate: z.string(),
+        endDate: z.string().optional(),
+      })
+    ),
+    order: z.number(), // For sorting experience items
+  }),
+});
+
+export const collections = { projects, experience };
